ebd16c1f3736d0f82fae0e195ba9f77fd2d25956
unknown <dhanupriasokan@gmail.com>
2023-11-24 21:15:23 +0530

    usecase2

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,9 +1,51 @@
+package Employee_1;
+class Company{
+    private static  int wage_per_hour ;
+    private static int full_day_hour;
+
+
+    Company(int wage_per_hour,int full_day_hour){
+        this.wage_per_hour=wage_per_hour;
+        this.full_day_hour=full_day_hour;
+
+
+    }
+
+
+    public static int getWage_per_hour() {
+        return wage_per_hour;
+    }
+
+    public static void setWage_per_hour(int wage_per_hour) {
+        Company.wage_per_hour = wage_per_hour;
+    }
+
+    public static int getFull_day_hour() {
+        return full_day_hour;
+    }
+
+    public static void setFull_day_hour(int full_day_hour) {
+        Company.full_day_hour = full_day_hour;
+    }
+}
+
 public class Employee_Wage {
-    
+
     public static int  generate_random(){
         int present_or_absent= (int) Math.floor((Math.random()*10)%3);
         return present_or_absent;
-        
+
+    }
+    public static int calculateDaily_Wage(int presentOrAbsent,Company company){
+        if(presentOrAbsent==1 || presentOrAbsent==2) {
+            return  Company.getWage_per_hour() * Company.getFull_day_hour();
+        }
+        else{
+            return 0;
+        }
+
+
+
     }
 
     public static void main(String[] args){
@@ -16,6 +58,12 @@ public class Employee_Wage {
             System.out.println("present");
         }
 
+        Company company1=new Company(20,8);
+        int Daily_wage=calculateDaily_Wage(presentOrabsent,company1);
+        System.out.println(Daily_wage);
+
+
     }
 
 }
+
