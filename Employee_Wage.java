597dadcd23f03639f47936724008a5ead1b52905
unknown <dhanupriasokan@gmail.com>
2023-11-24 21:14:02 +0530

    usecase1

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,8 +1,21 @@
-public class Employee_Wage{
+public class Employee_Wage {
+    
+    public static int  generate_random(){
+        int present_or_absent= (int) Math.floor((Math.random()*10)%3);
+        return present_or_absent;
+        
+    }
 
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation Program");
-        
+        int presentOrabsent=generate_random();
+        if( presentOrabsent==0){
+            System.out.println("Absent");
+        }
+        else{
+            System.out.println("present");
+        }
+
     }
 
 }
